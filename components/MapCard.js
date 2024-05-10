73b25cce203897a3221a2d4ebc7329207a917867
Sharjeel Hussain <sharjeelhussain877@gmail.com>
2024-05-11 03:27:23 +0500

    new changes for current location of user

--- components/MapCard.js
+++ components/MapCard.js
@@ -1,138 +1,234 @@
-import { View, Text ,SafeAreaView, StyleSheet, TextInput, Button} from 'react-native'
-import React, { useState } from 'react'
-import { FontAwesome6 ,MaterialIcons,
-} from '@expo/vector-icons';
-import MapLocate from './../components/MapLocate';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
+import MapLocate from "./../components/MapLocate";
+import * as Location from "expo-location";
+
 export default function mapCard() {
-  const [val,setVal] =useState('')
-  const [whereTo,setWhereto] =useState('')
-// console.log(whereTo)
+  const [val, setVal] = useState("");
+  const [whereTo, setWhereto] = useState("");
+  const [data, setData] = useState([]);
+  const [location, setLocation] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const getLocation = async () => {
+    console.log("triggered --> ", location)
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+      return;
+    }
+    try {
+      let locationData = await Location.getCurrentPositionAsync({});
+      // let arr = []
+      // arr = arr.push(locationData)
+      setLocation([...location, locationData]);
+      setErrorMsg(null);
+    } catch (error) {
+      console.error("Error getting location: ", error);
+      setErrorMsg("Error getting location. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const handleGetLocation = () => {
+    getLocation();
+  };
+
+
+  const fetchData = async (location) => {
+    try {
+      const response = await fetch(
+        `https://api.foursquare.com/v3/places/search?near=${encodeURIComponent(
+          location
+        )}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "fsq3bDfgMuZCsrSM5ulfMj3ZPmDZcebZ9X7ZRp2mFJllaNk=",
+            Accept: "application/json",
+          },
+        }
+      );
+      const res = await response.json();
+      const updateData = res?.results?.slice(0, 8).map((data) => {
+        let obj = {
+          longitude: data.geocodes?.main?.longitude,
+          latitude: data.geocodes?.main?.latitude,
+          id: data.fsq_id,
+          distance: data.distance,
+          location: data.location?.address,
+          name: data.name,
+        };
+        return obj;
+      });
+      setData(updateData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData(val, whereTo);
+  }, [val]);
+
   return (
     <View style={styles.mapContainer}>
       <View style={styles.mapView}>
-      <MapLocate val={val} whereTo={whereTo}/>
+        <MapLocate
+          val={val}
+          whereTo={whereTo}
+          // data={data}
+          location={location}
+        />
       </View>
       <View style={styles.mapModal}>
-        {/* <Text style={{fontSize:25,fontWeight:"bold",textAlign:"center",borderBlockColor:"#c3c3c3",borderBottomWidth:.5,paddingBottom:10}}>Good Morning, Ubaid</Text> */}
-      <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={(e)=>setVal(e)}
-        value={val}
-        placeholder='your location'
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(e)=>setWhereto(e)}
-        value={whereTo}
-        placeholder='Where to go'
-
-      />
-      </SafeAreaView>
-      { !val ? <Button title='use my location' />:
-      <Button title='search'/>
-        
-      }
-      <View  style={styles.card1}>
-            <View >
-        <MaterialIcons name="group-work" size={25} color="#000" />
-            </View>
-        <View>
+        <SafeAreaView>
+          <View style={styles.texts}>
+            <TextInput
+              style={styles.input}
+              onChangeText={(e) => setVal(e)}
+              value={val}
+              placeholder="your location"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={(e) => setWhereto(e)}
+              value={whereTo}
+              placeholder="Where to go"
+            />
+            {!val ? (
+              <Button
+                title="use my location"
+                onPress={handleGetLocation}
+              />
+            ) : (
+              <Button title="search" />
+            )}
+          </View>
+        </SafeAreaView>
+        <View style={styles.card1}>
+          <View>
+            <MaterialIcons name="group-work" size={25} color="#000" />
+          </View>
+          <View>
             <Text style={styles.bold}>Work</Text>
             <Text style={styles.light}>1024,Market Street</Text>
+          </View>
         </View>
-        </View>
-        <View  style={styles.card2}>
-            <View style={{marginTop:18}}>
-        <FontAwesome6 name="street-view" size={25} color="#000" />
-            </View>
-        <View>
+        <View style={styles.card2}>
+          <View style={{ marginTop: 18 }}>
+            <FontAwesome6 name="street-view" size={25} color="#000" />
+          </View>
+          <View>
             <Text style={styles.bold}>burnsRoad Street</Text>
             <Text style={styles.light}>Karachi</Text>
+          </View>
         </View>
-        </View>
-        </View>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    mapContainer:{
-        display:"flex",
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+  mapContainer: {
+    display: "flex",
 
-        // backgroundColor:"blue"
-
+    // backgroundColor:"blue"
+  },
+  mapView: {
+    flex: 0.5,
+    // backgroundColor:"red"
+  },
+  mapModal: {
+    flex: 0.5,
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    padding: 10,
+    // margin:10,
+    borderTopWidth: 1,
+    borderTopColor: "transparent",
+    shadowColorshadowColor: "#000", // Shadow color
+    shadowOffset: {
+      width: 0,
+      height: 10,
     },
-    mapView:{
-        flex:0.5,
-        // backgroundColor:"red"
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    elevation: 5,
+    // backgroundColor:"green"
+  },
+  texts: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: "20px",
+  },
+  input: {
+    height: 50,
+    width: 340,
+    borderWidth: 2,
+    padding: 10,
+    margin: 10,
+    borderBlockColor: "#c3c3c3",
+    borderRadius: 10,
+  },
+  card1: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    // marginTop:50,
+    alignItems: "center",
+    gap: 9,
+    height: 70,
+    width: 387,
+    padding: 10,
 
-    },
-    mapModal:{
-        flex:0.5,
-        width: '100%',
-        height: '100%',
-        display:"flex",
-        justifyContent:"center",
-        padding:10,
-        // margin:10,
-        borderTopWidth: 1, 
-        borderTopColor: 'transparent',
-        shadowColorshadowColor: '#000', // Shadow color
-        shadowOffset: {
-          width: 0,
-          height: 10,
-        },
-        shadowOpacity: 0.3,
-        shadowRadius: 10,
-        elevation: 5, 
-        // backgroundColor:"green"
-
-    },
-    input: {
-        height: 50,
-        width:370,
-        margin: 12,
-        borderWidth: 2,
-        // padding: 10,
-        // borderBlockColor:"#c3c3c3",
-        borderRadius:10,
-      },
-      card1:{
-        width:"100%",
-        display:"flex",
-        flexDirection:"row",
-        // marginTop:50,
-        alignItems:"center",
-        gap:9,
-        height:70,
-        width:387,
-        padding:10,
-
-        borderBottomColor:"#c3c3c3",
-        borderBottomWidth:.5,
-      backgroundColor: '#F6F6F6',
-      },
-      card2:{
-          width:387,
-          padding:10,
-          display:"flex",
-          flexDirection:"row",
-        //   alignItems:"center",
-          gap:9,
-          height:75,
-          borderBottomColor:"#c3c3c3",
-          borderBottomWidth:.5,
-          backgroundColor: '#F6F6F6',
-      },
-      bold:{
-          fontSize:23,
-          fontWeight:"bold",
-          marginTop:9,
-  
-      },
-      light:{
-          fontSize:12,
-          color:"#D9D9D9"
-      },
-})
+    borderBottomColor: "#c3c3c3",
+    borderBottomWidth: 0.5,
+    backgroundColor: "#F6F6F6",
+  },
+  card2: {
+    width: 387,
+    padding: 10,
+    display: "flex",
+    flexDirection: "row",
+    //   alignItems:"center",
+    gap: 9,
+    height: 75,
+    borderBottomColor: "#c3c3c3",
+    borderBottomWidth: 0.5,
+    backgroundColor: "#F6F6F6",
+  },
+  bold: {
+    fontSize: 23,
+    fontWeight: "bold",
+    marginTop: 9,
+  },
+  light: {
+    fontSize: 12,
+    color: "#D9D9D9",
+  },
+});
--- components/MapLocate.js
+++ components/MapLocate.js
@@ -1,61 +1,21 @@
-import React, { useEffect, useState,useCallback } from 'react';
-import { View, Text, SafeAreaView, TextInput, StyleSheet } from 'react-native';
-import MapView, { Marker, Polyline } from 'react-native-maps';
-import { debounce } from 'lodash';
-export default function MapLocate({ val, whereTo }) {
-  const [data, setData] = useState([])
+import React, { useEffect, useState, useCallback } from "react";
+import { View, Text, SafeAreaView, TextInput, StyleSheet } from "react-native";
+import MapView, { Marker, Polyline } from "react-native-maps";
 
-  
-       const fetchData = async (location) => {
-       console.log("success")
-        try {          
-            const response = await fetch(`https://api.foursquare.com/v3/places/search?near=${encodeURIComponent(location)}`, 
-            {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'fsq3bDfgMuZCsrSM5ulfMj3ZPmDZcebZ9X7ZRp2mFJllaNk=',
-                    'Accept': 'application/json'
-                }
-            });
-            const res = await response.json();
-            const updateData = res?.results?.slice(0, 8).map(data => {
-                let obj = {
-                    lon: data.geocodes?.main?.longitude,
-                    lat: data.geocodes?.main?.latitude,
-                    id: data.fsq_id,
-                    distance: data.distance,
-                    location: data.location?.address,
-                    name:data.name,
-                }
-                return obj
-            })
-            setData(updateData)
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-    const debounced = React.useCallback(debounce(fetchData, 5000), []);
+export default function MapLocate({ val, whereTo, data,location }) {
 
-    React.useEffect(() => {
-      // for the first render load
-      fetchData(val,whereTo);
-    }, [val]);
-    
-console.log(whereTo);
   return (
     <View style={styles.container}>
       <MapView style={styles.map}>
-        {data?.map(marker => ( 
-          <Marker 
+        {data?.map((marker) => (
+          <Marker
             key={marker?.id}
-            coordinate={{ latitude: marker?.lat, longitude: marker?.lon }}
+            coordinate={{ latitude: marker?.latitude, longitude: marker?.longitude }}
             title={marker?.name}
             description={marker?.location}
           />
         ))}
-        
       </MapView>
-       
     </View>
   );
 }
